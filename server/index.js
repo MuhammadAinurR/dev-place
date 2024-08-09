2736609462006dddb7d13c6109849417d182ae33
Muhammad Ainur Rofiq <rofiq@192.168.1.3>
2024-08-09 12:59:42 +0700

    update

--- server/index.js
+++ server/index.js
@@ -10,3 +10,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 module.exports = app;
+
+// module.exports = {
+//     apps: [{
+//         name: "devPlace-server",
+//         script: "./index.js",
+//         env: {
+//             JWT_SECRET: 'super-secret-code',
+//             GITHUB_CLIENT_ID: 'Ov23liby8cjlxlqUTDba',
+//             GITHUB_CLIENT_SECRET: '9746601b2f85aaf0d503621e2075c7c985c057e3',
+//             GOOGLE_AUDIENCE: '307747605125-ipj32ju7dca41cr242o3qbc42a668h3g.apps.googleusercontent.com',
+//             GEMINI_API_KEY: 'AIzaSyBizlceZsGFo4-CoXxcRl-ghY-1iF99Os8',
+
+//             PORT: 80,
+//             NODE_ENV: 'production',
+//             DB_URL: 'postgresql://postgres.xcwekvotvtlsequybsax:Oaoc4t7TSEl5724m@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres',
+//         }
+//     }]
+// }
